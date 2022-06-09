2a2376086d23e2759e664ef6df1b88bd663de2b3
Chutiphonpond <support@altotech.net>
2022-06-09 23:51:37 +0700

    second commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+import { FaBars, FaHome, FaWindowClose } from "react-icons/fa";
+import { useState } from "react";
+import './Navigation.css'
+
+const Navigation = () => {
+    const [showMenu,setShowMenu] = useState(false)
+    const toggleMenu = () => setShowMenu(!showMenu)
+  return (
+    <aside>
+      <div className="navbar">
+        <div className="navbar-toggle">
+          <Link to="#" className="menu-bar">
+            <FaBars onClick={toggleMenu}/>
+          </Link>
+        </div>
+      </div>
+      <nav className={showMenu ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu-item">
+          <li className="navbar-toggle">
+            <Link>
+              <FaWindowClose onClick={toggleMenu}/>
+            </Link>
+          </li>
+          <li className="menu-text">
+            <Link to="#">
+              <FaHome />
+              <span>หน้าแรก</span>
+            </Link>
+          </li>
+          <li className="menu-text">
+            <Link to="#">
+              <FaHome />
+              <span>หน้าแรก</span>
+            </Link>
+          </li>
+          <li className="menu-text">
+            <Link to="#">
+              <FaHome />
+              <span>หน้าแรก</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
+};
+
+export default Navigation;
